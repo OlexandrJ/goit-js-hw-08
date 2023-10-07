82daebae31a5a3f8d3811d0ee8be9ebf3f4e81ee
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-10-07 11:54:45 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,23 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-const STORAGE_KEY = 'videoplayer-current-time';
-const saveTimeToStorage = (currentTime) => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTime));
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+const saveCurrentTime = (time) => {
+    localStorage.setItem('videoplayer-current-time', time);
+};
+const restoreCurrentTime = () => {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime) {
+        player.setCurrentTime(parseFloat(currentTime));
+    }
 };
 
-const loadTimeFromStorage = () => {
-    const savedTime = localStorage.getItem(STORAGE_KEY);
-    return savedTime ? JSON.parse(savedTime) : 0;
-};
-
-player.setCurrentTime(loadTimeFromStorage());
-player.on('timeupdate', throttle((data) => {
-    saveTimeToStorage(data.seconds);
+player.on('timeupdate', throttle((event) => {
+    const currentTime = event.seconds;
+    saveCurrentTime(currentTime);
 }, 1000));
+
+window.addEventListener('load', () => {
+    restoreCurrentTime();
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,39 @@
 import throttle from 'lodash.throttle';
 
-function saveFormDataToLocalStorage(event) {
-  const formData = {
-    email: event.target.querySelector('input[name="email"]').value,
-    message: event.target.querySelector('textarea[name="message"]').value,
-  };
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
-
-function populateFormFieldsFromLocalStorage() {
-  const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-  if (savedFormData) {
-    const emailInput = document.querySelector('input[name="email"]');
-    const messageTextarea = document.querySelector('textarea[name="message"]');
-
-    emailInput.value = savedFormData.email;
-    messageTextarea.value = savedFormData.message;
-  }
-}
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-  localStorage.removeItem('feedback-form-state');
-  
-  const emailInput = document.querySelector('input[name="email"]');
-  const messageTextarea = document.querySelector('textarea[name="message"]');
-
-  emailInput.value = '';
-  messageTextarea.value = '';
-
+function saveFormState() {
   const formData = {
     email: emailInput.value,
     message: messageTextarea.value,
   };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
 
+function loadFormState() {
+  const formData = localStorage.getItem('feedback-form-state');
+  if (formData) {
+    const { email, message } = JSON.parse(formData);
+    emailInput.value = email;
+    messageTextarea.value = message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageTextarea.value = '';
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
   console.log(formData);
 }
 
-const feedbackForm = document.querySelector('.feedback-form');
-feedbackForm.addEventListener('input', throttle(saveFormDataToLocalStorage, 500));
-feedbackForm.addEventListener('submit', handleFormSubmit);
+form.addEventListener('input', throttle(saveFormState, 500));
+form.addEventListener('submit', handleSubmit);
 
-populateFormFieldsFromLocalStorage();
+loadFormState();
